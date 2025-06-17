68dc09af49048ed9494bfa806e56c0d37c6646b2
sulyman002 <oyedelesulaiman@gmail.com>
2025-06-17 11:05:19 +0100

    getting closer

---
+++ script.js
@@ -0,0 +1,20 @@
+module.exports = {
+  theme: {
+    extend: {
+      fontFamily: {
+        barlow: ['Barlow', 'sans-serif'],
+      },
+    },
+  },
+}
+
+
+module.exports = {
+  theme: {
+    extend: {
+      fontFamily: {
+        fraunces: ['Fraunces', 'serif'],
+      },
+    },
+  },
+}
